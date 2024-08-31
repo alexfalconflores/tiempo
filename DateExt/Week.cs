96a2115aad05d30902268a44675b06b59c2e6f1f
Alex Stefano Falcon Flores <alexfalconflores@hotmail.com>
2024-08-31 15:42:38 -0500

    [ADD] Week

---
+++ DateExt/Week.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DateExt;
+
+public static class Week
+{
+    public static DateTime AddWeeks(this DateTime dateTime, int amount)
+    {
+        int days = amount * 7;
+        return dateTime.AddDays(days);
+    }
+}
