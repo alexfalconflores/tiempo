1978a442ddf04764e86d089bf2977279746a50f5
Alex Stefano Falcon Flores <alexfalconflores@hotmail.com>
2024-09-01 04:54:14 -0500

    [ADD] DifferenceInBusinessDays

--- DateExt/DayExt.cs
+++ DateExt/DayExt.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Xml.Schema;
 
 namespace DateExt;
 
@@ -57,12 +58,12 @@ public static class DayExt
     /// from the dates and then the difference in days is calculated.
     /// <example><code>
     /// // How many calendar days are between
-    /// // 31 august, 2024 06:00:00 and 30 august, 2024 00:00:00
+    /// // 31 August, 2024 06:00:00 and 30 August, 2024 00:00:00
     /// var res = new DateTime(2024, 8, 31, 6, 0, 0).DifferenceInCalendarDays(new DateTime(2024, 8, 30));
     /// // -> 1
     /// 
     /// // How many calendar days are between
-    /// // 31 august, 2024 06:00:00 and 31 august, 2023 00:00:00
+    /// // 31 August, 2024 06:00:00 and 31 August, 2023 00:00:00
     /// var res = new DateTime(2024, 8, 31, 6, 0, 0).DifferenceInCalendarDays(new DateTime(2023, 8, 31));
     /// // -> 366
     /// </code></example>
@@ -76,5 +77,53 @@ public static class DayExt
         DateTime startOfDayRight = dateTimeRight.StartOfDay();
         TimeSpan diff = startOfDayLeft - startOfDayRight;
         return diff.Days;
+    }
+    /// <summary>
+    /// Get the number of business day between the given dates.
+    /// Businnes days being days that aren't in the weekend.
+    /// Like <see cref="DifferenceInCalendarDays(DateTime, DateTime)"/>, the function removes the times from
+    /// the dates before calculating the difference.
+    /// <example><code>
+    /// // How many business days are between
+    /// // 31 August, 2024 06:00:00 and 29 August, 2024 00:00:00
+    /// var res = new DateTime(2024, 8, 31, 6, 0, 0).DifferenceInCalendarDays(new DateTime(2024, 8, 29));
+    /// // -> 2
+    /// 
+    /// // How many business days are between
+    /// // 31 August, 2024 06:00:00 and 31 August, 2023 00:00:00
+    /// var res = new DateTime(2024, 8, 31, 6, 0, 0).DifferenceInCalendarDays(new DateTime(2023, 8, 31));
+    /// // -> 262
+    ///
+    /// // How many business days are between
+    /// // 1 November, 2021 06:00:00 and 1 December, 2021 00:00:00
+    /// var res = new DateTime(2021, 11, 1, 6, 0, 0).DifferenceInCalendarDays(new DateTime(2021, 12, 1));
+    /// // -> -22
+    /// </code></example>
+    /// </summary>
+    /// <param name="dateTimeLeft">The later date</param>
+    /// <param name="dateTimeRight">The earlier date</param>
+    /// <returns>The number of business days</returns>
+    public static int DifferenceInBusinessDays(this DateTime dateTimeLeft, DateTime dateTimeRight)
+    {
+        if (IsSameDay(dateTimeLeft, dateTimeRight)) return 0;
+
+        bool isNegative = dateTimeLeft < dateTimeRight;
+        if (isNegative) (dateTimeLeft, dateTimeRight) = (dateTimeRight, dateTimeLeft);
+
+        int calendarDifference = dateTimeLeft.DifferenceInCalendarDays(dateTimeRight);
+
+        int fullWeeks = calendarDifference / 7;
+        int businessDays = fullWeeks * 5;
+
+        dateTimeRight = dateTimeRight.AddDays(fullWeeks * 7);
+
+        while (!IsSameDay(dateTimeLeft, dateTimeRight))
+        {
+            if (!dateTimeRight.IsWeekend())
+                businessDays++;
+            dateTimeRight = dateTimeRight.AddDays(1);
+        }
+
+        return isNegative ? -businessDays : businessDays;
     }
 }
