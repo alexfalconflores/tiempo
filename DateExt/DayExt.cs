238397e3f1dcceec0052c375b5dc2831cafa01cb
Alex Stefano Falcon Flores <alexfalconflores@hotmail.com>
2024-09-01 00:21:27 -0500

    [ADD] IsSameDay and StartOfDay

--- DateExt/DayExt.cs
+++ DateExt/DayExt.cs
@@ -16,4 +16,40 @@ public static class DayExt
     /// <param name="days">The amount of days to be subtracted</param>
     /// <returns>The new <see cref="DateTime"/> with the days subtracted</returns>
     public static DateTime SubtractDays(this DateTime dateTime, int days) => dateTime.AddDays(-(days));
+    /// <summary>
+    /// Are the given dates in the same day (and year and month)?
+    /// <example>
+    /// <code>
+    /// //Are 31 August 06:00:00 and 31 August 18:00:00 in the same day?
+    /// var result = new DateTime(2024, 8, 31, 6,0,0).IsSameDay(new DateTime(2024, 8, 31, 18, 0,0));
+    /// // -> True
+    /// 
+    /// //Are 30 August and 30 September in the same day?
+    /// var result = new DateTime(2024, 8, 31, 6,0,0).IsSameDay(new DateTime(2024, 8, 31, 18, 0,0));
+    /// // -> False
+    /// 
+    /// //Are 31 August, 2024 and 31 August, 2023 in the same day?
+    /// var result = new DateTime(2024, 8, 31, 6,0,0).IsSameDay(new DateTime(2024, 8, 31, 18, 0,0));
+    /// // -> False
+    /// </code>
+    /// </example>
+    /// </summary>
+    /// <param name="dateTime1">The first date to check</param>
+    /// <param name="dateTime2">The second date to check</param>
+    /// <returns>The date are in the same day (and year and month)</returns>
+    public static bool IsSameDay(this DateTime dateTimeLeft, DateTime dateTimeRight) =>
+        dateTimeLeft.StartOfDay().Equals(dateTimeRight.StartOfDay());
+    /// <summary>
+    /// Return the start of a day for the given date.
+    /// <example>
+    /// <code>
+    /// //The start of a day for 31 August 2024 06:00:00
+    /// var result = new DateTime(2024, 8, 31, 6,0,0).StartOfDay();
+    /// // -> 8/31/2024 12:00:00 AM
+    /// </code>
+    /// </example>
+    /// </summary>
+    /// <param name="dateTime">The original date</param>
+    /// <returns>The start of a day</returns>
+    public static DateTime StartOfDay(this DateTime dateTime) => dateTime.Date;
 }
